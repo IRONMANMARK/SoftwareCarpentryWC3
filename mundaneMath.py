0aac77f0f0baded50c59013564f3260d2c1b9f3d
IRONMANMARK <33693731+IRONMANMARK@users.noreply.github.com>
2019-02-14 14:55:48 -0500

    Update mundaneMath.py

--- mundaneMath.py
+++ mundaneMath.py
@@ -1,6 +1,6 @@
 def add(lower_bound, upper_bound):
     a = 0
     for i in range(upper_bound-lower_bound + 1):
-        a += i + 1
+        a += lower_bound + i
     return a
 print(add(int(input('Enter Lower bound: ')), int(input('Enter upper bound: '))))
